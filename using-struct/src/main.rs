45eb56584e0cb2997b5dd56da20caa11cf13e89f
pauloalchemist <paulolinsdev@gmail.com>
2021-01-24 23:53:02 -0300

    width and height of a rectangle specified in pixels and calculate the area of the rectangle

--- using-struct/src/main.rs
+++ using-struct/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 60;
+    
+    println!(
+        "A área do retângulo é {} pixels quadrado",
+        area(width1, height1)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
