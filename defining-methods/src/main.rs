63e43aa1a48c1c7f52bfaa51daf043f33452d12f
pauloalchemist <paulolinsdev@gmail.com>
2021-01-31 13:37:39 -0300

    methods with more parameters

--- defining-methods/src/main.rs
+++ defining-methods/src/main.rs
@@ -9,6 +9,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, ohter: &Rectangle) -> bool {
+        self.width > ohter.width && self.height > ohter.height
+    }
 }
 
 fn main() {
@@ -16,5 +20,18 @@ fn main() {
         width: 50,
         height: 70
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 60
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 80
+    };
+
     println!("Area of the rectangle is {} square pixels", rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
+
+
