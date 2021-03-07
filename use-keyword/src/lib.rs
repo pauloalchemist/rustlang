a7e4665b3395b7a325aa6f234e48ff6a03a7573f
pauloalchemist <paulolinsdev@gmail.com>
2021-03-07 18:34:09 -0300

    use with std

--- use-keyword/src/lib.rs
+++ use-keyword/src/lib.rs
@@ -1,13 +1,6 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
+use std::collections::HashMap;
 
-use crate::front_of_house::hosting;
-
-pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 }
