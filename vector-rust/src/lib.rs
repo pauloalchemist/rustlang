aaa7e59b925b2b6112fbb63f2d84799a804e4996
pauloalchemist <paulolinsdev@gmail.com>
2021-03-20 22:02:04 -0300

    read vector with Rust

--- vector-rust/src/lib.rs
+++ vector-rust/src/lib.rs
@@ -1,3 +1,13 @@
+#![allow(unused)]
+
 fn main() {
-    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element"),
+    }
 }
